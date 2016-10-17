6f8249a6ec3b0105108c7525aa609f863f535717
Hooligan-0 <gwen-github@agilack.fr>
2016-10-17 16:10:29 +0200

    = ADD: create basic data model for Rotation

---
+++ data-model/rotation.cpp
@@ -0,0 +1,164 @@
+/*
+ * This file is part of VLE, a framework for multi-modeling,
+ * simulation and analysis of complex dynamical systems.
+ * http://www.vle-project.org
+ *
+ * Copyright (c) 2016 Agilack
+ */
+#include "atelier.h"
+#include "rotation.h"
+
+/**
+ * @brief Default constructor for Rotation object
+ *
+ */
+Rotation::Rotation(const QString &name, ulong duration)
+{
+    mDuration = duration;
+    mName     = name;
+}
+
+/**
+ * @brief Default destructor for an Rotation object
+ *
+ */
+Rotation::~Rotation()
+{
+    // Nothing to do
+}
+
+/**
+ * @brief Add a new activity plan into the Rotation
+ *
+ * @param position Position of the new plan into the Rotation
+ * @param name Name of the new plan
+ * @return Pointer to the newly created ActivityPlan
+ */
+ActivityPlan *Rotation::addPlan(ulong position, const QString &name)
+{
+    // Create a new ActivityPlan
+    ActivityPlan *newPlan = new ActivityPlan(position, name);
+    // Insert it to the current Rotation
+    mPlans.push_back(newPlan);
+    // ... and return it
+    return newPlan;
+}
+
+/**
+ * @brief Add a new activity plan into the Rotation, with a default name
+ *
+ * @param position Position of the new plan into the Rotation
+ * @return Pointer to the newly created ActivityPlan
+ */
+ActivityPlan *Rotation::addPlan(ulong position)
+{
+    return addPlan(position, "NewPlan");
+}
+
+/**
+ * @brief Get the number of activity plans into the Rotation
+ *
+ * @return Number of available activity plans
+ */
+uint Rotation::countPlans(void)
+{
+    return mPlans.count();
+}
+
+/**
+ * @brief Get the Rotation duration
+ *
+ * @return ulong
+ */
+ulong Rotation::getDuration(void)
+{
+    return mDuration;
+}
+
+/**
+ * @brief Get the Rotation name
+ *
+ * @return QString
+ */
+const QString &Rotation::getName(void)
+{
+    return mName;
+}
+
+/**
+ * @brief Get the plan at 'index' position
+ *
+ * @param index Index of the plan
+ * @return Pointer to the specified plan
+ */
+ActivityPlan *Rotation::getPlan(int index)
+{
+    if (index > (mPlans.count() - 1))
+        return 0;
+
+    return mPlans.at(index);
+}
+
+/**
+ * @brief Remove one activity plan from Rotation
+ *
+ * @param index Index of the plan to remove
+ */
+void Rotation::removePlan(int index)
+{
+    if (index > (mPlans.count() - 1))
+        return;
+
+    // Take the specified activity plan from Rotation
+    ActivityPlan *p = mPlans.takeAt(index);
+    // Delete it
+    delete p;
+}
+
+/**
+ * @brief Set the duration of this Rotation
+ *
+ * @param duration Number of years of the Rotation
+ */
+void Rotation::setDuration(ulong duration)
+{
+    mDuration = duration;
+}
+
+/**
+ * @brief Set the name of this Rotation
+ *
+ * @param name String of the name to set
+ */
+void Rotation::setName(const QString &name)
+{
+    mName = name;
+}
+
+// -------------------- Activity Plans --------------------
+
+ActivityPlan::ActivityPlan(ulong position, const QString &name)
+{
+    mPosition = position;
+    mName = name;
+}
+
+QString ActivityPlan::getName(void)
+{
+    return mName;
+}
+
+ulong ActivityPlan::getPosition(void)
+{
+    return mPosition;
+}
+
+void ActivityPlan::setName(const QString &name)
+{
+    mName = name;
+}
+
+void ActivityPlan::setPosition(ulong position)
+{
+    mPosition = position;
+}
---
+++ data-model/rotation.h
@@ -0,0 +1,50 @@
+/*
+ * This file is part of VLE, a framework for multi-modeling,
+ * simulation and analysis of complex dynamical systems.
+ * http://www.vle-project.org
+ *
+ * Copyright (c) 2016 Agilack
+ */
+#ifndef ROTATION_H
+#define ROTATION_H
+
+#include <QList>
+#include <QString>
+#include <QtGlobal>
+
+class ActivityPlan;
+
+class Rotation
+{
+public:
+    explicit Rotation(const QString &name, ulong duration = 0);
+    ~Rotation();
+    ActivityPlan *addPlan(ulong position, const QString &name);
+    ActivityPlan *addPlan(ulong position);
+    uint  countPlans(void);
+    ulong getDuration(void);
+    const QString &getName(void);
+    ActivityPlan *getPlan(int index);
+    void removePlan(int index);
+    void setDuration(ulong duration);
+    void setName(const QString &name);
+private:
+    QString mName;
+    ulong   mDuration;
+    QList<ActivityPlan *> mPlans;
+};
+
+class ActivityPlan
+{
+public:
+    explicit ActivityPlan(ulong position, const QString &name);
+    QString getName(void);
+    ulong   getPosition(void);
+    void    setName(const QString &name);
+    void    setPosition(ulong position);
+private:
+    ulong   mPosition;
+    QString mName;
+};
+
+#endif // ROTATION_H
